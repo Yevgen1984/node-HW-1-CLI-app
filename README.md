cb5dee1e7fc08d49daf9410f8684f6d20c7177cf
Yevgen <107062952+Yevgen1984@users.noreply.github.com>
2022-12-04 12:32:39 +0200

    HW 1 (node.js)

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # node-HW-1-CLI-app
+
+https://ibb.co/GpzSK2Y
+https://ibb.co/k5zgbYB
+https://ibb.co/F70BV1D
+https://ibb.co/1bPGcj0
---
+++ contacts.js
@@ -0,0 +1,65 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.resolve("db/contacts.json");
+
+async function listContacts() {
+  const list = await fs
+    .readFile(contactsPath)
+    .then((data) => JSON.parse(data))
+    .catch((err) => console.log(err.message));
+  return list;
+}
+
+async function getContactById(contactId) {
+    const contact = await fs
+    .readFile(contactsPath)
+    .then((data) => JSON.parse(data))
+    .then((data) => data.find((el) => el.id === contactId))
+
+    .catch((err) => console.log(err.message));
+
+  if (!contact) {
+    return "There is no requested id";
+  }
+  return contact;
+}
+
+async function removeContact(contactId) {
+    const contact = await fs
+    .readFile(contactsPath)
+    .then((data) => JSON.parse(data))
+    .then((data) => {
+      if (data.find((el) => el.id === contactId)) {
+        return data.filter((el) => el.id !== contactId);
+      }
+      return null;
+    })
+    .catch((err) => console.log(err.message));
+
+  if (!contact) {
+    return "There is no requested id";
+  }
+  await fs.writeFile(contactsPath, JSON.stringify(contact, null, 3));
+  return contact;
+}
+
+async function addContact(name, email, phone) {
+    const newContact = { name, email, phone, id: nanoid() };
+    const contacts = await fs
+      .readFile(contactsPath)
+      .then((data) => JSON.parse(data))
+      .catch((err) => console.log(err.message));
+  
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 3));
+    return contacts;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
